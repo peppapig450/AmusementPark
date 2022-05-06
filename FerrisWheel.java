6a3fe7b524694c2c730a6da2747628edcb64e50b
Nick Brady <55930078+peppapig450@users.noreply.github.com>
2022-05-06 20:19:48 +0000

    ferris wheel

---
+++ FerrisWheel.java
@@ -0,0 +1,7 @@
+public class FerrisWheel extends Attraction {
+
+  public FerrisWheel(int mRider, int tCost, int tNum, int aNum, int rLength, int rPerHour, int hPerDay, double speed, double height) {
+    super(mRider, tCost, tNum, aNum, rLength, rPerHour, hPerDay, speed, height); 
+  }
+
+}
