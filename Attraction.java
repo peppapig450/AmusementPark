bd474783ffc3baf03aa198fbb26a15d29619daa7
Nick Brady <55930078+peppapig450@users.noreply.github.com>
2022-05-06 19:35:19 +0000

    Initial commit

---
+++ Attraction.java
@@ -0,0 +1,37 @@
+public class Attraction {
+    private int maxRiders;
+    private int ticketCost;
+    private int ticketNum;
+    private int attendantNum;
+    private int rideLength;
+    private int ridersPerHour;
+    private int hoursPerDay;
+    private double speed;
+    private double height;
+
+    public Attraction(int mRider, int tCost, int tNum, int aNum, int rLength, int rPerHour, int hPerDay, double speed, double height) {
+    this.maxRiders = mRider;
+    this.ticketCost = tCost;
+    this.ticketNum = tNum;
+    this.attendantNum = aNum;
+    this.rideLength = rLength;
+    this.ridersPerHour = rPerHour;
+    this.hoursPerDay = hPerDay;
+    this.speed = speed;
+    this.height = height;
+    }
+
+    public int getMaxRiders() {
+        return maxRiders;
+    }
+
+    public int getTicketCost() {
+        return ticketCost;
+    }
+
+    // retur
+    public int getTicketNum() {
+        return ticketNum;
+    }
+    public int get
+}
